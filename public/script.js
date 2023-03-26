3ac12a0db55ac1d36ea3055626961fd4f62c0ff8
mujugen <101939481+mujugen@users.noreply.github.com>
2023-03-26 22:13:55 +0800

    Working cv summarizer

--- public/script.js
+++ public/script.js
@@ -1,3 +1,92 @@
+// Function to convert PDF to text
+function processPdf(blob) {
+  return new Promise(async (resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const typedArray = new Uint8Array(event.target.result);
+      try {
+        const pdfDoc = await pdfjsLib.getDocument(typedArray).promise;
+        const numPages = pdfDoc.numPages;
+        let rawText = "";
+
+        for (let i = 1; i <= numPages; i++) {
+          const page = await pdfDoc.getPage(i);
+          const textContent = await page.getTextContent();
+          rawText += textContent.items.map((item) => item.str).join(" ");
+        }
+
+        resolve({ text: rawText });
+      } catch (error) {
+        reject(error);
+      }
+    };
+    reader.onerror = (event) => {
+      reject(event.target.error);
+    };
+    reader.readAsArrayBuffer(blob);
+  });
+}
+// End of processPdf
+
+// Call CVSummarize API endpoint
+async function fetchCVSummarize(pdfText) {
+  const response = await fetch("http://localhost:3000/cvsummarize", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ pdfText }),
+  });
+  const applicant = await response.json();
+  return applicant;
+}
+// End of fetchCVSummarize
+
+// Function for Convert to Text Button
+async function convertUploadedFiles() {
+  // Get the list of uploaded files from the server
+  const response = await fetch("http://localhost:3000/list");
+  const files = await response.json();
+
+  for (const file of files) {
+    const filePath = `uploads/${file}`;
+    const name = file;
+
+    try {
+      const pdfBytes = await fetch(filePath).then((response) =>
+        response.arrayBuffer()
+      );
+      const { x, text } = await processPdf(new Blob([pdfBytes]));
+      console.log("PDF converted");
+      const applicant = await fetchCVSummarize(text);
+      const name = applicant.name;
+      const education = applicant.education;
+      const skills = applicant.skills;
+      const experience = applicant.experience;
+      const certifications = applicant.certification;
+      const accomplishments = applicant.accomplishment;
+      console.log(JSON.stringify(applicant));
+      // Create an HTML container for the raw text
+      const container = $(`
+        <div class="pdf-container mt-4">
+          <h3>${name}</h3>
+          <div class="pdf-text"><b>Education:</b> ${education}</div>
+          <div class="pdf-text"><b>Skills:</b> ${skills}</div>
+          <div class="pdf-text"><b>Experience:</b> ${experience}</div>
+          <div class="pdf-text"><b>Certifications:</b> ${certifications}</div>
+          <div class="pdf-text"><b>Accomplishments:</b> ${accomplishments}</div>
+        </div>
+      `);
+
+      // Append the container to the page
+      $("#pdfContainers").append(container);
+    } catch (error) {
+      console.error(`Error processing file: ${file}`, error);
+    }
+  }
+}
+
+// Event listeners to load when document is ready
 $(document).ready(function () {
   $("#uploadForm").on("submit", function (event) {
     event.preventDefault();
@@ -30,5 +119,10 @@ $(document).ready(function () {
         alert("Failed to upload files.");
       },
     });
+  });
+  // Event listener for convert button
+  $("#convertBtn").on("click", function () {
+    console.log("Convert button clicked");
+    convertUploadedFiles();
   });
 });
--- server.js
+++ server.js
@@ -2,12 +2,145 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Configuration, OpenAIApi } = require("openai");
 // Create an Express app
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+// Initialization of openai API variable
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+// End of open AI initialization
+
+// Function that takes the text prompt and puts it into OpenAI API
+// Returns text response
+async function askPrompt(prompt) {
+  const model_engine = "text-davinci-003";
+
+  console.log("Waiting for response");
+  const response = await openai.createCompletion({
+    model: model_engine,
+    prompt: prompt,
+    temperature: 0,
+    max_tokens: 1000,
+    n: 1,
+    top_p: 1.0,
+  });
+  console.log("Response received\n");
+  return response.data.choices[0].text;
+}
+// End of askPrompt function
+
+// Function that takes any text and returns lines that match the match text
+// Returned matched text if found, and null if not
+function featureRetrieval(match_text, text) {
+  const lines = text.split("\n");
+  for (const line of lines) {
+    if (line.includes(match_text)) {
+      const line_without_match = line.replace(match_text, "");
+      return line_without_match.trim();
+    }
+  }
+  return null;
+}
+// End of featureRetrieval()
+
+// Container for Applicant details
+class Applicant {
+  constructor(
+    name,
+    education = [],
+    experience = [],
+    skills = [],
+    certification = [],
+    accomplishment = []
+  ) {
+    this.name = name;
+    this.education = education;
+    this.skills = skills;
+    this.experience = experience;
+    this.certification = certification;
+    this.accomplishment = accomplishment;
+  }
+
+  add_education(education) {
+    this.education.push(education);
+  }
+
+  add_experience(experience) {
+    this.experience.push(experience);
+  }
+
+  add_skill(skill) {
+    this.skills.push(skill);
+  }
+
+  add_certification(certification) {
+    this.certification.push(certification);
+  }
+
+  add_accomplishment(accomplishment) {
+    this.accomplishment.push(accomplishment);
+  }
+}
+// End of Applicant Class
+
+// Main function that will ask a prompt to openAI API, retrieve a response, and extract the features
+// Creates and returns an Applicant class with the found features
+async function CVSummarize(text) {
+  const pdfText = text;
+  const prompt = `${pdfText}\n\n\n\n\n
+    can you find only these information and put them in this format,
+    Full Name:
+    Education:
+    Experience:
+    Skills:
+    Certifications:
+    Accomplishments:
+    `;
+
+  const response = await askPrompt(prompt);
+  const fullName = featureRetrieval("Full Name:", response);
+  const education = featureRetrieval("Education:", response);
+  const experience = featureRetrieval("Experience:", response);
+  const skills = featureRetrieval("Skills:", response);
+  const certifications = featureRetrieval("Certifications:", response);
+  const accomplishments = featureRetrieval("Accomplishments:", response);
+
+  const applicant = new Applicant(
+    fullName,
+    education,
+    experience,
+    skills,
+    certifications,
+    accomplishments
+  );
+  console.log("CVSummarize finished");
+  return applicant;
+}
+// End of CVSummarize()
+
+// Handle requests for CV summarization
+app.post("/cvsummarize", async (req, res) => {
+  const { pdfText } = req.body;
+  try {
+    const applicant = await CVSummarize(pdfText);
+    res.json(applicant);
+  } catch (error) {
+    console.error("Error processing CV:", error);
+    res.status(500).send("Failed to process CV.");
+  }
+});
+
 // Set the public folder as a static folder
 app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
 // Configure Multer storage
 const storage = multer.diskStorage({
@@ -28,6 +161,16 @@ app.post("/upload", upload.array("files"), (req, res) => {
     return res.status(400).send("No files were uploaded.");
   }
   res.send("Files uploaded successfully.");
+});
+
+// Route for listing uploaded files
+app.get("/list", (req, res) => {
+  fs.readdir("uploads", (err, files) => {
+    if (err) {
+      return res.status(500).send("Failed to list uploaded files.");
+    }
+    res.json(files);
+  });
 });
 
 // Start the server on port 3000
